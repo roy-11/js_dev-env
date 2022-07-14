d6a19989718779a51408c10a15d51e48dfd830b2
roy-11 <63440731+roy-11@users.noreply.github.com>
2022-07-14 11:46:01 +0900

    feat: add webpack dev setting

--- scripts/server.js
+++ scripts/server.js
@@ -1,9 +1,18 @@
 import express from "express";
 import open from "open";
+import webpack from "webpack";
+import config from "../webpack.config.dev";
 // import path from "path";
 
 const app = express();
 const port = process.env.PORT || 3000;
+const compiler = webpack(config);
+
+app.use(
+  require("webpack-dev-middleware")(compiler, {
+    publicPath: config.output.publicPath,
+  })
+);
 
 // app.get("/", (req, res) => {
 //   res.sendFile(path.join(__dirname, "../src/index.html"));
